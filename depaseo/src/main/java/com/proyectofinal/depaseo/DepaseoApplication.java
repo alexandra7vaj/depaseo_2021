c6616c986163eda0c23637f3412abbf37307e339
Alexa <Alexa@DESKTOP-0FDB2GT>
2021-06-16 23:54:08 +0200

    ProyectoFinal_JavaSpring_DePaseo

---
+++ depaseo/src/main/java/com/proyectofinal/depaseo/DepaseoApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.depaseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepaseoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepaseoApplication.class, args);
+	}
+
+}
---
+++ depaseo/src/test/java/com/proyectofinal/depaseo/DepaseoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.depaseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepaseoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
