71ea834b51a111f29bc9d0c886ae016ebbf47949
admin CIFO <admin CIFO@DESKTOP-K9FH0BE>
2021-07-06 13:47:15 +0200

    generatedvalue added

--- depaseo/src/main/java/controller/Route.java
+++ depaseo/src/main/java/controller/Route.java
@@ -1,9 +1,14 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -11,71 +16,72 @@ import javax.persistence.Table;
 public class Route {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	public String name;
-	public String type; // tipo Gastranomico, Historico, Arquitectónico, familiar, festeo...
+	public String type; // Gastronomico, Historico, Arquitectónico, familiar, Ocio Nocturno...
 	public int location; 
-	public HashMap<String, Route> routes; // este hashmap nos permitirá guardar objetos con las informaciones
+	//public HashMap<String, Route> routes; // este hashmap nos permitirá guardar objetos con las informaciones
                                           //de geolocalización y asociarlos a un tag.
-
-// **********Constructor***********************************************************
 	
-public Route(int id, String name, String type, int location, HashMap<String, Route> routes) {
-	super();
-	//this.id = id; innecesario porque el @id de JPA ya me lo crea
-	this.name = name;
-	this.type = type;
-	this.location = location;
-	this.routes = new HashMap <String, Route>(); //¿?
 	
-}
-
-//faltaba el constructor vacio:
-
-public Route() {
-	super();}
-
-// *******Getters && Setters**************
-
+	//Defining relationship type: Many to One
+	@ManyToOne 
+	@JoinColumn (name = "ID_User")
+	private User user;
 	
-
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public String getType() {
-	return type;
-}
-public void setType(String type) {
-	this.type = type;
-}
-public int getLocation() {
-	return location;
-}
-public void setLocation(int location) {
-	this.location = location;
-}
-public HashMap<String, Route> getRoutes() {
-	return routes;
-}
-public void setRoutes(HashMap<String, Route> routes) {
-	this.routes = routes;
-}
-
-//**********ToString***************************************
-
-@Override
-public String toString() {
-	return "Route [id=" + id + ", name=" + name + ", type=" + type + ", location=" + location + ", routes=" + routes
-			+ "]";
-    }
-}
+	//--------Constructor-------------
+	
+	public Route(String name, String type, int location, User user) {
+		super();
+		this.name = name;
+		this.type = type;
+		this.location = location;
+		this.user = user;
+	}	
+			
+	public Route() {
+		super();}
+	
+		
+	//-----Getters and Setters----------
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getLocation() {
+		return location;
+	}
+	public void setLocation(int location) {
+		this.location = location;
+	}
+	/*public HashMap<String, Route> getRoutes() {
+		return routes;
+	}
+	public void setRoutes(HashMap<String, Route> routes) {
+		this.routes = routes;
+	}*/
+	
+	//------ToString------------
+	
+	@Override
+	public String toString() {
+		return "Route [id=" + id + ", name=" + name + ", type=" + type + ", location=" + location + "]";
+	    }
+	}
 
