9f7ad5bd02c51b406b69c64824762d17774b27f2
admin CIFO <admin CIFO@DESKTOP-K9FH0BE>
2021-07-07 13:30:57 +0200

    commit dimecres

--- depaseo/src/main/java/controller/RouteController.java
+++ depaseo/src/main/java/controller/RouteController.java
@@ -1,74 +1,123 @@
 package controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
 
-@RequestMapping ("/routecontroller")
+@RequestMapping("/routecontroller")
 public class RouteController {
-	
-@Autowired
-RouteRepository routeRepository;
+
+	@Autowired
+	RouteRepository routeRepository;
 
 
-//-----------CRUD----------------
-//-------------------------------
-//-------------------------------
-	
-//-----------Create----------------
-	@RequestMapping ("/newroute")
-public String newRoute() {
-		return "newroute";
+	//-----------CRUD----------------
+
+//List or Get all:
+
+	@RequestMapping("/getallroutes")
+	public String getAllRoutes(Model boxToView) {
+
+		boxToView.addAttribute("routesfromController", routeRepository.findAll());
+		return "getallroutes";
+
+	}
+
+//Create:
+	@RequestMapping("/newroute")
+	public String newRoute() {
 		
-	}	
+		return "newroute";
 
-@RequestMapping ("/addroute")
-public String addRoute(Route route) { //nos faltaba también, enviar el objeto route para poder hacer .save)
-	
-	routeRepository.save(route);
-	
-	return "addroute";	
-}
+	}
 
+	@RequestMapping("/addroute")
+	public String addRoute(Route route) { 
 
-	//-----------Update----------------Necesitamos hacer 2 pasos: find (si no lo encuentra, notfound) & replace
-@RequestMapping ("/updateroute")
-public String updateRoute() {
-	return "updateroute";		
-}
+		routeRepository.save(route);
 
+		return "addroute";
+	}
 
-	//-----------Delete----------------Necesitamos hacer 2 pasos: find & delete
-@RequestMapping ("/deleteroute")
-public String deleteroute() {
-	return "deleteroute";
-	
-}
-
-	//-----------List----------------
-@RequestMapping ("/listroutes")
-public String listRoutes() {
-	return "listroutes";
-	
-
-
-//--------------------------------------------------------------------------------
-	//------------------------- service to controller --------------------------------
-	//---------------------------Change to find route-----------------------------------------------------
-
-		//public Optional<Expense> findOneExpenseById(int id) {
-
-			// System.out.println("inside findEmployee" + id);
-			//Optional<Expense> expenseFound = expenseRepository.findById(id);
-			// System.out.println("finishing findEmployee" + id);
-			// System.out.println("finishing findEmployee" + employeeFound.get());
-			//return expenseFound;
+//Update:
+	//2 steps: find and replace information
+	@RequestMapping("/updateroute")
+	public String updateRoute(int id, Model model) {
+		
+		Optional<Route> routeFound = findOneRouteById(id);
+		
+		if (routeFound.isPresent()) {
+			
+			model.addAttribute("routefromController", routeFound.get());
+			return "updateroute";
+		
 		}
 		
+		return "notfound";
+	}
+	
+	@RequestMapping("/modifyroute") 
+	public String modifyRoute(int id, Route route){
+		return null;}
+	
+	/*@PostMapping("/replaceExpense/{idFromView}")
+	public String replaceEmployee(@PathVariable("idFromView") int id, Expense expense, Model model) {
 
+		Optional<Expense> expenseFound = findOneExpenseById(id);
 
+		if (expenseFound.isPresent()) {
 
+			if (expense.getName() != null)
+				expenseFound.get().setName(expense.getName());
+			
+			
+			if (expense.getValue() != 0.0)
+				expenseFound.get().setValue(expense.getValue());
+
+			expenseRepository.save(expenseFound.get());
+			model.addAttribute("expensesfromController", expenseRepository.findAll());
+			model.addAttribute("expenseUpdated", expenseFound.get());
+			
+			return "expenses.html";
+
+		} else
+			return "notfound.html";
+	}*/
+	
+
+// Delete
+	//2 steps: find & delete
+	@RequestMapping("/deleteroute")
+	public String deleteroute(int id, Model model) {
+		Optional<Route> routeFound = routeRepository.findById(id);
+		
+		if (routeFound.isPresent()) {
+			routeRepository.deleteById(id);
+			//model.addAttribute() mirar proyecto employee
+			
+			return "deletedroute";
+		}
+		
+		return "notfound";
+
+	}
+
+// Service to controller:
+
+	public Optional<Route> findOneRouteById(int id) {
+
+		Optional<Route> routeFound = routeRepository.findById(id);
+		return routeFound;
+	}
+	
+	public String notFound (Model model) {
+		return "notfound";
+		
+	}
 
 }
--- depaseo/src/main/java/controller/UserController.java
+++ depaseo/src/main/java/controller/UserController.java
@@ -1,63 +1,80 @@
-	package controller;
-	import org.springframework.beans.factory.annotation.Autowired;
-	import org.springframework.stereotype.Controller;
-	import org.springframework.web.bind.annotation.RequestMapping;
+package controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 
 @RequestMapping("/usercontroller")
 
 public class UserController {
-		
+
 	@Autowired
-	UserRepository userRepository;	
-	
+	UserRepository userRepository;
+
 //-----------CRUD----------------
-//-------------------------------
-//-------------------------------			
-		
-		
-//-----------Create----------------
-@RequestMapping("/newuser")
-public String newUser() {
 
-			return "newuser.html";
-		}
+	// Create:
+	@RequestMapping("/newuser")
+	public String newUser() {
 
-@RequestMapping("/adduser")
-public String inserUser(User user) {
+		return "newuser.html";
+	}
+
+	@RequestMapping("/adduser")
+	public String inserUser(User user) {
 
 		userRepository.save(user);
 
-			return "redirect:/employee/allEmployees";
-		}
-		
-		
-//-----------Update----------------	
-@RequestMapping("/updateuser")
-public String updateUser() {
-	return "updateuser";}
+		return "?"; // importante decidir a donde queremos que nos redirija.
+	}
 
-	//to change userName
-	@RequestMapping ("/changeusername")
-	public String changeUserName (){
-		return "changeusername";} 
-	
-	//to change password
-	@RequestMapping ("/changepassword")		
-	public String changePassword (){
-		return "changepassword";} 
-		
-		
-//-----------Delete----------------
-@RequestMapping ("/deleteaccount")
-public String deleteAccount (){
-	return "deleteaccount";}
+//Update:
+	@RequestMapping("/updateuser")
+	public String updateUser(Model model, int id) {
 
-		
-//-----------List (not necessary)--
-		
-			
+		Optional<User> userFound = userRepository.findById(id);
 
+		if (userFound.isPresent()) {
+			model.addAttribute("userfromController", userFound.get());
+			return "updateuser";
 		}
 
+		return "notfound";
+	}
+
+	@PostMapping("/modifyUser/{idFromView}")
+	public String modifyUser(@PathVariable("idFromView") int id, User user) {
+
+		Optional<User> userFound = userRepository.findById(id);
+
+		if (userFound.isPresent()) {
+// to change userName:
+			if (user.getName() != null)
+				userFound.get().setName(user.getName());
+
+// to change password:
+			if (user.getPassword() != null)
+				userFound.get().setPassword(user.getPassword());
+
+			userRepository.save(userFound.get());
+			return "?"; // decidir a donde nos redirige
+
+		} else
+			return "notfound";
+
+	}
+
+//Delete:
+	@RequestMapping("/deleteaccount")
+	public String deleteAccount() {
+		return "deleteaccount";
+	}
+
+}
